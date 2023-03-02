2d8ea802524f95213eef1caec21e63e169d5f982
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2023-03-02 19:45:01 +0200

    add some examples

---
+++ src/detectors/yolo.cpp
@@ -0,0 +1,11 @@
+#include "detectors.hpp"
+
+std::function<std::vector<detectors::FaceLandmarks>(image_tools::Image)>
+yolo_factory(detectors::Model *model) {
+  // do some preparations
+  return
+      [model](
+          image_tools::Image image) -> std::vector<detectors::FaceLandmarks> {
+        // do other things to construct landmarks from an image
+      };
+}
