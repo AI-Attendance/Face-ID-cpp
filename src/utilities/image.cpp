9a8c5ebda0435a34bd1229b7e05f3d38c9ea7749
Ahmed Abdelaziz <102368041+AhmedAbdelaziz0@users.noreply.github.com>
2023-03-11 21:20:52 +0200

    Add files via upload

--- src/utilities/image.cpp
+++ src/utilities/image.cpp
@@ -34,13 +34,13 @@ void image_tools::read_from_disk(Image& dst, const std::string& dir)
 		std::cout << "Error happen when reading image!\n";
 }
 
-void image_tools::save_to_disk(Image& src, const std::string& dir)
+void image_tools::save_to_disk(const Image& src, const std::string& dir)
 {
 	cv::imwrite(dir, src);
 }
 
 // tested
-void image_tools::resize(Image& src, Image& dst, int height, int width)
+void image_tools::resize(const Image& src, Image& dst, int height, int width)
 {
 	float f = src.size().aspectRatio() * ((float)height / width);
 	if(src.size().aspectRatio() > (float(width) / height))
@@ -48,4 +48,47 @@ void image_tools::resize(Image& src, Image& dst, int height, int width)
 	else
 		width = int(width * f);
 	cv::resize(src, dst, cv::Size(width, height));
+}
+
+//tested
+void image_tools::pad(const Image &src, Image &dst, int height, int width)
+{
+	if (src.size().height > height || src.size().width > width)
+		return;
+
+	if (src.size[0] & 1 || src.size[1] & 1)
+		cv::resize(src, src, cv::Size(2 * src.size[0] / 2, 2 * src.size[0] / 2));
+
+	int top = (height - src.size().height) / 2;
+	int left = (width - src.size().width) / 2;
+	cv::copyMakeBorder(src, dst, top, top, left, left, cv::BORDER_CONSTANT);
+}
+
+#include <iostream>
+
+bool image_tools::test_face(const Image& src, const float black_thres, const float blur_thres)
+{
+	// test 1
+	// if the image has too much black padding
+	Image inverted, gray;
+	cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+	float presentage_of_black = 1 - cv::countNonZero(gray) /
+										(float)(gray.size().width * gray.size().height);
+	if(presentage_of_black > black_thres)
+		return false;
+
+	// test 2
+	// if the image is blurry
+	// gray mat from above code
+	// depend on the size of the frame
+	Image laplacian;
+	cv::Scalar mean, stddev;
+    Laplacian(gray, laplacian, CV_64F);
+	cv::meanStdDev(laplacian, mean, stddev);
+	float sdv = stddev.val[0] * stddev.val[0];
+	std::cout << "SDV: " << sdv << std::endl;
+	if (sdv < blur_thres)
+		return false;
+
+	return true;
 }
