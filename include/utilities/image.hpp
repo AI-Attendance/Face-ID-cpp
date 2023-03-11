4031d61999b81d13ef253e8da408fa5473db1c54
Ahmed Abdelaziz <102368041+AhmedAbdelaziz0@users.noreply.github.com>
2023-03-11 21:20:24 +0200

    Add files via upload

--- include/utilities/image.hpp
+++ include/utilities/image.hpp
@@ -4,16 +4,19 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
 
 namespace image_tools {
 // TODO: add struct contents
-	#define Image cv::Mat
-	void resize(Image& src, Image& dst, int hight, int width);
-	void pad(Image& src, Image& dst, int hight, int width);
-	void read_from_cam(Image& src, const std::string& ip_cam);
-	void read_from_cam(Image& src, const int ip_cam);
+	//typedef cv::Mat Image;
+#define Image cv::Mat
+	void resize(const Image& src, Image& dst, int hight, int width);
+	void pad(const Image& src, Image& dst, int hight, int width);
+	void read_from_cam(Image& dst, const std::string& ip_cam);
+	void read_from_cam(Image& dst, const int ip_cam);
 	char display(const Image& src, int wait_time, const std::string& winname);
 	void read_from_disk(Image& dst, const std::string& dir);
-	void save_to_disk(Image& src, const std::string& dir);
+	void save_to_disk(const Image& src, const std::string& dir);
 	void distroyWin(const std::string& winname);
+	bool test_face(const Image& src, const float black_thres=0.2, const float blur_thres=100); // before running face recognition
 } // namespace image_tools
